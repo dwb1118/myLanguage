af99f4e1650e057927ddf742cef874ac1299b8a7
Daniel Bell <danielwittbell@outlook.com>
2024-03-21 12:38:21 -0500

    a

--- myLanguage.py
+++ myLanguage.py
@@ -7,3 +7,20 @@ class BasicLexer(Lexer):
     tokens = { NAME, NUMBER, STRING }
     ignore = '\t '
     literals = {'=', '+', '-', '/', '*', '(', ')', ',', ';'}
+
+    NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    STRING = r'\".*?\"'
+
+    @_(r'\d+')
+    def NUMBER(self, t):
+
+        t.value = int(t.value)
+        return t
+    
+    @_(r'//.*')
+    def COMMENT(self, t):
+        pass
+
+    @_(r'\n+')
+    def newline(self, t):
+        self.lineno = t.value.count('\n')
